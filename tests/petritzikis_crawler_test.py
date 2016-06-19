a0241dda0c43842a2c30699977d1465431bc6f85
ipinak <giannis.pin@gmail.com>
2016-06-19 20:20:38 +0200

    parses and downloads a recipe's content

--- tests/petritzikis_crawler_test.py
+++ tests/petritzikis_crawler_test.py
@@ -2,39 +2,66 @@
 # -*- coding: utf-8 -*-
 import pytest
 
-from xekatharisma.petritzikis_crawler import PetritzikisRecipeDiscoverer
+from xekatharisma.petritzikis_crawler import PetritzikisRecipeDiscoverer,\
+    PetritzikisRecipeParser
 
 
 def test_get_food_category():
     p = PetritzikisRecipeDiscoverer()
     result = p.fetch_menu_links()
+    p.cleanup()
+
     assert result is not None or len(result) < 80
     assert result.pop().startswith('http')
-    p.cleanup()
+    print(result)
 
 
 def test_get_links_in_page():
     p = PetritzikisRecipeDiscoverer()
     result = p.fetch_links_in_page(
         'http://akispetretzikis.com/en/tags/moschari')
-    assert result is not None
-    assert result.pop().startswith('http')
     p.cleanup()
 
-@pytest.mark.only
+    assert result is not None
+    assert result.pop().startswith('http')
+    print(result)
+
+
 def test_get_pages_links_in_page():
     p = PetritzikisRecipeDiscoverer()
     result = p.fetch_pages_in_page(
         'http://akispetretzikis.com/en/tags/moschari')
-    assert result[0].startswith('http')
     p.cleanup()
 
+    assert result[0].startswith('http')
+    print(result)
 
-@pytest.mark.only
+
 def test_get_more_pages_links_in_page():
     p = PetritzikisRecipeDiscoverer()
     result = p.fetch_pages_in_page(
         'http://akispetretzikis.com/en/tags/kotopoylo')
-    assert result[0].startswith('http')
-    print result
     p.cleanup()
+
+    assert result[0].startswith('http')
+    print(result)
+
+
+@pytest.mark.only
+def test_get_recipt_in_page():
+    p = PetritzikisRecipeParser()
+    url = 'http://akispetretzikis.com/en/categories/almyres-pites-tartes/agglikh-kotopita'
+    result = p.fetch_recipe(url)
+    p.cleanup()
+
+    ing = result.get('ingredients', None)
+    time = result.get('time', None)
+    ex = result.get('execution', None)
+    imgs = result.get('images', None)
+
+    assert ing is not None and time is not None
+    assert ing[0] is not None and '' not in ing
+    assert ex[0] is not None and '' not in ex
+    assert imgs[0] is not None and '' not in imgs
+    assert imgs[0].startswith('http://akis')
+    print(result)
--- xekatharisma/petritzikis_crawler.py
+++ xekatharisma/petritzikis_crawler.py
@@ -16,14 +16,13 @@ class PetritzikisRecipeDiscoverer(Parser):
         self.browser.get(self.BASE_URL)
         menu = self.browser.find_element_by_id('menu')
         sub_menu = menu.find_elements_by_class_name('sub')
-        all_links = [link.get_attribute('href')\
-                     for menu_item in sub_menu\
-                        for link in menu_item.find_elements_by_tag_name('a')\
-                            if 'javascript' not in link.get_attribute('href')
+        all_links = [link.get_attribute('href')
+                     for menu_item in sub_menu
+                     for link in menu_item.find_elements_by_tag_name('a')
+                     if 'javascript' not in link.get_attribute('href')
                      ]
         # remove duplicates
-        links_sets = set(all_links)
-        return links_sets
+        return set(all_links)
 
     def fetch_links_in_page(self, url):
         """Return the links to recipes in paginated pages"""
@@ -31,25 +30,15 @@ class PetritzikisRecipeDiscoverer(Parser):
         self.browser.get(url)
         main = self.browser.find_element_by_id('tag')\
             .find_elements_by_tag_name('div')
-        all_links = [link.get_attribute('href')\
+        all_links = [link.get_attribute('href')
                      for links in main
-                        for link in links.find_elements_by_tag_name('a')
-                    ]
-        aset = set(all_links)
-        return aset
+                     for link in links.find_elements_by_tag_name('a')]
+        return set(all_links)
 
     def fetch_pages_in_page(self, url):
         """Returns the links that point to the pages
         <nav class="pagination">
-            <span class="page">
-                <a rel="next" href="/en/tags/moschari?page=2">2</a>
-            </span>
-            <span class="page">
-                <a href="/en/tags/moschari?page=3">3</a>
-            </span>
-            <span class="page">
-            <a href="/en/tags/moschari?page=4">4</a>
-            </span>
+            ...
             <span class="page">
                 <a href="/en/tags/moschari?page=5">5</a>
             </span>
@@ -72,11 +61,43 @@ class PetritzikisRecipeDiscoverer(Parser):
 
         return self._generate_pages(last_link, last_page)
 
-
     def _generate_pages(self, url, last_page):
         """Generate the pages based on a schema:
             http://domain.com/something?page={num}"""
 
         url_without_num = url[:-1]
-        return ["{0}{1}".format(url_without_num, page)\
+        return ["{0}{1}".format(url_without_num, page)
                 for page in xrange(2, last_page+1)]
+
+
+class PetritzikisRecipeParser(Parser):
+
+    def __init__(self):
+        Parser.__init__(self)
+
+    def fetch_recipe(self, url):
+        """Fetch the recipe i.e. ingredients, execution, images"""
+
+        self.browser.get(url)
+
+        main = self.browser.find_element_by_class_name('recipe-main')
+        ingredients = main.find_elements_by_class_name('text')[2]\
+            .find_elements_by_tag_name('li')
+        time = main.find_element_by_class_name('time')\
+            .find_element_by_class_name('value').text
+        ingredients_items = [item.text for item in ingredients]
+
+        method_el = main.find_elements_by_class_name('text')[1]\
+            .find_elements_by_tag_name('li')
+        method_items = [item.text for item in method_el]
+
+        images = main.find_element_by_class_name('ipad_media')\
+            .find_elements_by_tag_name('img')
+        images_items = [img.get_attribute('src') for img in images]
+
+        return {
+            'ingredients': ingredients_items,
+            'time': time,
+            'execution': method_items,
+            'images': images_items
+        }
